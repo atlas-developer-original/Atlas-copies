8b2a77709444397e82c457682c5619b2a69095c6
𝐌𝐑 𝐀𝐓𝐋𝐀𝐒 <afodilzr32@gmail.com>
2026-03-16 00:59:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports={
     probot_ids :'282859044593598464',//أيدي بروبوت
-    recipientId : '1056523152957636608',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
-    price : '100000',//سعر النسخ
-    serverid : '1384893060671864897',//أيدي سيرفارك
-    token : 'MTQ1MzI5MjEyMTYyNzk1MTI5Ng.GiYeb6.SXRKdM2ulwDUI_e4LYe9TX2V1VLVSaSaQfR9so',//توكن بوتك
-    purchaseLogChannelId: '1399278745122242612', // روم عمليات الشراء
-    logsChannelId: '1399278745122242612' // روم السجلات
+    recipientId : '1409552886286782484',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
+    price : '315790',//سعر النسخ
+    serverid : '1466049956329820318',//أيدي سيرفارك
+    token : 'MTQ4MjQ1MjYzOTYyNjI5NzM2NQ.GEgqks.yQx18aTSy7fT0zprOY71fUqCylPAjG9gNZxr6c',//توكن بوتك
+    purchaseLogChannelId: '1482784339833131070', // روم عمليات الشراء
+    logsChannelId: '1482784339833131070' // روم السجلات
 }
